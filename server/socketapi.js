d57bab91a105d21cac3066cb0159526e757e4d82
Marco <marco.riopel@hotmail.com>
2021-02-06 13:55:09 -0500

    Add socket io basic chat implementation server side

--- server/socketapi.js
+++ server/socketapi.js
@@ -1,11 +1,20 @@
 
-const io = require("socket.io")();
+const io = require("socket.io")({
+    cors: {
+        origin: "*",
+    }
+});
 const socketapi = {
     io: io
 };
 
 io.on("connection", function (socket) {
     console.log("A user connected");
+
+    socket.on('message', (message) => {
+        console.log(message);
+        io.emit('message', `${socket.id.substr(0, 2)} said ${message}`);
+    });
 });
 
 module.exports = socketapi;
