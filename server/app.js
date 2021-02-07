848b1d929dee7964237de97de751646e80a1f61e
Marco <marco.riopel@hotmail.com>
2021-02-06 19:57:47 -0500

    Add mongodb basic setup

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var databaseService = require('./services/database.service');
 
 var indexRouter = require('./routes/index');
 
@@ -39,15 +40,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+databaseService.startDB();
+
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ server/services/database.service.js
@@ -0,0 +1,14 @@
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://" + process.env.MONGO_USERNAME + ":" + process.env.MONGO_PASSWORD + "@cluster0.oia6z.mongodb.net/database?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+
+async function startDB() {
+    try {
+        await client.connect();
+    } catch (e) {
+        console.error(e);
+    }
+    console.log("We are on the moon");
+}
+
+module.exports = { startDB };
