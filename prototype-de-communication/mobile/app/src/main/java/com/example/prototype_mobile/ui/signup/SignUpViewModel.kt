071f344e1ba98775e112cd915ba3b49d2c9f27cf
Julien <julien.witty@polymtl.ca>
2021-02-11 04:06:28 -0500

    Account creation working but can't login after signin

--- prototype-de-communication/mobile/app/src/main/java/com/example/prototype_mobile/ui/signup/SignUpViewModel.kt
+++ prototype-de-communication/mobile/app/src/main/java/com/example/prototype_mobile/ui/signup/SignUpViewModel.kt
@@ -1,15 +1,21 @@
 package com.example.prototype_mobile.ui.signup
 
 import android.content.Context
+import android.content.Intent
 import android.util.Patterns
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.android.volley.RequestQueue
 import com.example.prototype_mobile.R
+import com.example.prototype_mobile.data.LoginRepository
 import com.example.prototype_mobile.data.Result
 import com.example.prototype_mobile.data.Signup.SignupDataSource
+import com.example.prototype_mobile.data.model.LoggedInUser
+import com.example.prototype_mobile.ui.chat.ChatActivity
 import com.example.prototype_mobile.ui.login.LoggedInUserView
 import com.example.prototype_mobile.ui.login.LoginFormState
 import com.example.prototype_mobile.ui.login.LoginResult
@@ -17,6 +23,7 @@ import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 import okhttp3.Dispatcher
 
+//https://developer.android.com/kotlin/coroutines
 
 //this class may need to be migrated in LoginViewModel since a lot of the methos are repeating
 //theirself and follow basically the same architecture.
@@ -30,13 +37,26 @@ class SignUpViewModel(private val signupDataSource: SignupDataSource) : ViewMode
     val loginFormState: LiveData<LoginFormState> = _signupForm
 
     private val _signupResult = MutableLiveData<LoginResult>()
-    val loginResult: LiveData<LoginResult> = _signupResult
+    val signupResult: LiveData<LoginResult> = _signupResult
 
     fun createAccount(username:String, password: String) {
         // can be launched in a separate asynchronous job
         viewModelScope.launch(IO)
         {
-            val result = signupDataSource.createAccount(username, password)
+            val result: Result<LoggedInUser> = try {
+                signupDataSource.createAccount(username, password)
+            } catch (e: Exception) {
+                Result.Error("Network request failed")
+            }
+
+            when(result) {
+                is Result.Success<LoggedInUser>
+                    ->  _signupResult.value =  LoginResult(success = LoggedInUserView(displayName = result.data.displayName))
+                else -> println("Erreur de creation de compte")
+
+            }
+
+
         }
 
         /*if (result is Result.Success) {
@@ -62,5 +82,6 @@ class SignUpViewModel(private val signupDataSource: SignupDataSource) : ViewMode
             username.isNotBlank()
         }
     }
+
 
 }
