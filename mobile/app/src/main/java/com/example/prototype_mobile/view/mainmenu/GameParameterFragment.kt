b082250ca2784cf7403430d7b12c69489c72cbc0
Julien <julien.witty@polymtl.ca>
2021-02-28 03:34:54 -0500

    listener debug
    Former-commit-id: b07fd078fc73397be8b45001d4b24f78c07ba9ec

--- mobile/app/src/main/java/com/example/prototype_mobile/view/mainmenu/GameParameterFragment.kt
+++ mobile/app/src/main/java/com/example/prototype_mobile/view/mainmenu/GameParameterFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.Nullable
 import com.example.prototype_mobile.R
+import com.example.prototype_mobile.databinding.FragmentGameCreationBinding
+import com.example.prototype_mobile.databinding.FragmentGameParameterBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,22 +22,34 @@ private const val ARG_PARAM2 = "param2"
  */
 class GameParameterFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var  binding: FragmentGameParameterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_parameter, container, false)
+        var view = inflater.inflate(R.layout.fragment_game_parameter, container, false)
+        return view
     }
+
+    override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentGameParameterBinding.bind(view)
+        binding.icognito.setOnClickListener{
+
+        }
+
+    }
+
+
 
     companion object {
         /**
@@ -50,9 +65,12 @@ class GameParameterFragment : Fragment() {
         fun newInstance() =
                 GameParameterFragment().apply {
                     arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
+
                     }
                 }
+    }
+
+    fun hello() {
+        println("hellooo")
     }
 }
