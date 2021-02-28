163ddfd2c63cbf151a7d58b118ccd31a029f55be
Julien <julien.witty@polymtl.ca>
2021-02-28 03:47:09 -0500

    dynamic toggle button
    Former-commit-id: 879e440c267db0ee2e03dbce28759d706f530259

--- mobile/app/src/main/java/com/example/prototype_mobile/view/mainmenu/GameParameterFragment.kt
+++ mobile/app/src/main/java/com/example/prototype_mobile/view/mainmenu/GameParameterFragment.kt
@@ -1,5 +1,6 @@
 package com.example.prototype_mobile.view.mainmenu
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -24,6 +25,7 @@ class GameParameterFragment : Fragment() {
     // TODO: Rename and change types of parameters
 
     private lateinit var  binding: FragmentGameParameterBinding
+    private var checked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,7 +46,9 @@ class GameParameterFragment : Fragment() {
 
         binding = FragmentGameParameterBinding.bind(view)
         binding.icognito.setOnClickListener{
-
+             hello()
+                binding.icognito.isActivated = ! binding.icognito.isActivated
+            
         }
 
     }
