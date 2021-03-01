1e30cae2e6f9b515e2301427d110df8e89905f81
Guillaume <g.thibault@polymtl.ca>
2021-03-01 17:57:57 -0500

    Added http request to get game list
    Former-commit-id: 7c5dd645306fe3fdf28e8c940ac76407c01c726a

--- desktop/src/app/services/search-game/search-game.service.ts
+++ desktop/src/app/services/search-game/search-game.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ACCESS } from '@app/classes/acces';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,31 +11,36 @@ export class SearchGameService {
   private gameList: any[] = []
   displayList: any[] = []
 
-  constructor() {
+  // URL
+  private baseUrl = environment.api_url;
+  private createGameUrl = this.baseUrl + "/api/games/list";
+  
+
+  constructor(private http: HttpClient) {
     // TODO: Get game list
-    this.gameList = [
-      {name: 'Game 1', type: 'classic', difficulty: 'normal'},
-      {name: 'Game 2', type: 'sprint', difficulty: 'hard'},
-      {name: 'Game 3', type: 'sprint', difficulty: 'easy'},
-      {name: 'Game 4', type: 'classic', difficulty: 'mushroom'},
-      {name: 'Game 5', type: 'classic', difficulty: 'hard'},
-      {name: 'Game 1', type: 'classic', difficulty: 'normal'},
-      {name: 'Game 2', type: 'sprint', difficulty: 'hard'},
-      {name: 'Game 3', type: 'sprint', difficulty: 'easy'},
-      {name: 'Game 4', type: 'classic', difficulty: 'mushroom'},
-      {name: 'Game 5', type: 'classic', difficulty: 'hard'},
-      {name: 'Game 1', type: 'classic', difficulty: 'normal'},
-      {name: 'Game 2', type: 'sprint', difficulty: 'hard'},
-      {name: 'Game 3', type: 'sprint', difficulty: 'easy'},
-      {name: 'Game 4', type: 'classic', difficulty: 'mushroom'},
-      {name: 'Game 5', type: 'classic', difficulty: 'hard'},
-      {name: 'Game 1', type: 'classic', difficulty: 'normal'},
-      {name: 'Game 2', type: 'sprint', difficulty: 'hard'},
-      {name: 'Game 3', type: 'sprint', difficulty: 'easy'},
-      {name: 'Game 4', type: 'classic', difficulty: 'mushroom'},
-      {name: 'Game 5', type: 'classic', difficulty: 'hard'},
-    ]
-    this.displayList = this.gameList;
+    // this.gameList = [
+    //   {name: 'Game 1', type: 'classic', difficulty: 'normal'},
+    //   {name: 'Game 2', type: 'sprint', difficulty: 'hard'},
+    //   {name: 'Game 3', type: 'sprint', difficulty: 'easy'},
+    //   {name: 'Game 4', type: 'classic', difficulty: 'mushroom'},
+    //   {name: 'Game 5', type: 'classic', difficulty: 'hard'},
+    //   {name: 'Game 1', type: 'classic', difficulty: 'normal'},
+    //   {name: 'Game 2', type: 'sprint', difficulty: 'hard'},
+    //   {name: 'Game 3', type: 'sprint', difficulty: 'easy'},
+    //   {name: 'Game 4', type: 'classic', difficulty: 'mushroom'},
+    //   {name: 'Game 5', type: 'classic', difficulty: 'hard'},
+    //   {name: 'Game 1', type: 'classic', difficulty: 'normal'},
+    //   {name: 'Game 2', type: 'sprint', difficulty: 'hard'},
+    //   {name: 'Game 3', type: 'sprint', difficulty: 'easy'},
+    //   {name: 'Game 4', type: 'classic', difficulty: 'mushroom'},
+    //   {name: 'Game 5', type: 'classic', difficulty: 'hard'},
+    //   {name: 'Game 1', type: 'classic', difficulty: 'normal'},
+    //   {name: 'Game 2', type: 'sprint', difficulty: 'hard'},
+    //   {name: 'Game 3', type: 'sprint', difficulty: 'easy'},
+    //   {name: 'Game 4', type: 'classic', difficulty: 'mushroom'},
+    //   {name: 'Game 5', type: 'classic', difficulty: 'hard'},
+    // ]
+    this.getGames();
   }
 
   fetchGameList(): void {
@@ -63,4 +71,22 @@ export class SearchGameService {
     this.gameList.forEach(game => game[key].toLowerCase() == value ? this.displayList.push(game): null);
   }
   
+  private getGames(): void {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'authorization': localStorage.getItem(ACCESS.TOKEN)!});
+    const options = { headers: headers };
+    this.http.get<any>(this.createGameUrl, options).subscribe(
+      res => {
+        console.log(res);
+        this.gameList = res.lobbies;
+        this.displayList = this.gameList;
+      },
+      err => {
+        console.log(err.error);
+      }
+    )
+
+  }
+
 }
