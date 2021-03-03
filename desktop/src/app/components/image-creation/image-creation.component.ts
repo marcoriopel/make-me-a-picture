cee1f115cc33f76a879cc40925bfb03ac1f3eb43
Marc-Alain Tétreault <marc-alain.tetreault@polymtl.ca>
2021-03-02 22:20:33 -0500

    added more validations to image creation form
    Former-commit-id: 51e41c589988f20080505018204542c8ba2774bf

--- desktop/src/app/components/image-creation/image-creation.component.ts
+++ desktop/src/app/components/image-creation/image-creation.component.ts
@@ -1,9 +1,11 @@
-  import { Component, OnInit} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 import {MatDialog} from '@angular/material/dialog';
 import { ViewingComponent } from '../viewing/viewing.component';
+import { HttpClient } from '@angular/common/http';
 import { Difficulty, Drawing } from '@app/classes/drawing';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-image-creation',
@@ -17,12 +19,14 @@ export class ImageCreationComponent implements OnInit {
   hint: string = '';
   hints: string[] = [];
   isHintListValid: boolean = false;
+  baseUrl = environment.api_url;
+  imageFormUrl = this.baseUrl + "/api/drawings/create"
 
   errorMessages: { [key: string]: string } = {
     text: "Entre 1 et 30 caractères",
   };
 
-  constructor(private fb: FormBuilder, public drawingService: DrawingService, public dialog: MatDialog) { }
+  constructor(private http: HttpClient, private fb: FormBuilder, public drawingService: DrawingService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.imageCreationForm = this.fb.group({
@@ -30,6 +34,7 @@ export class ImageCreationComponent implements OnInit {
         Validators.required,
         Validators.minLength(1),
         Validators.maxLength(30),
+        Validators.pattern(/^\S*$/),
       ]],
       difficulty:['', Validators.required],
     })
@@ -38,21 +43,33 @@ export class ImageCreationComponent implements OnInit {
         Validators.required,
         Validators.minLength(1),
         Validators.maxLength(30),
+        Validators.pattern(/^\S*$/),
       ]],
     })
   }
 
-  sendForm(): void {
-    const newImage: Drawing = {
+  async processForm() {
+    const drawing: Drawing = {
       drawingName: this.imageCreationForm.value.drawingName,
       difficulty: this.convertDifficulty(this.imageCreationForm.value.difficulty),
       strokes: this.drawingService.strokeStack,
       hints: this.hints,
     }
-    console.log(newImage);
+    this.sendDrawing(drawing).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      }
+    )
   }
 
   addHint(): void {
+    if(this.hints.includes(this.hintForm.value.hint)) {
+      alert('Vous avez déjà ajouté cet indice');
+      return;
+    }
     this.hints.push(this.hintForm.value.hint);
     this.isHintListValid = this.hints.length > 0 ? true : false;
     this.hintForm.reset();
@@ -64,7 +81,7 @@ export class ImageCreationComponent implements OnInit {
   }
 
   openPreview(): void {
-    const data: Drawing = {
+    const drawing: Drawing = {
       drawingName: this.imageCreationForm.value.drawingName,
       difficulty: this.convertDifficulty(this.imageCreationForm.value.difficulty),
       strokes: this.drawingService.strokeStack,
@@ -74,7 +91,7 @@ export class ImageCreationComponent implements OnInit {
     let dialogRef = this.dialog.open(ViewingComponent, {
       height: '800px',
       width: '1200px',
-      data: data,
+      data: drawing,
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -91,6 +108,10 @@ export class ImageCreationComponent implements OnInit {
       case 'Difficile': return Difficulty.Difficile;
     }
     return Difficulty.Facile // Default
+  }
+
+  sendDrawing(drawing: Drawing) {
+    return this.http.post<any>(this.imageFormUrl, drawing);
   }
 
 }
