03a48a24748bf6407aaf09eea78751429900d186
guthi1 <g.thibault@polymtl.ca>
2021-03-04 16:41:29 -0500

    First canvas that work
    Former-commit-id: 109b28213e3838afe8a38e4768eabfb705f9ecf4

--- mobile/app/src/main/java/com/example/prototype_mobile/view/game/CanvasFragment.kt
+++ mobile/app/src/main/java/com/example/prototype_mobile/view/game/CanvasFragment.kt
@@ -1,7 +1,11 @@
 package com.example.prototype_mobile.view.game
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.AttributeSet
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,10 +13,18 @@ import android.view.ViewGroup
 import com.example.prototype_mobile.R
 import com.example.prototype_mobile.viewmodel.game.CanvasViewModel
 
+
+// Inspired by: https://developer.android.com/codelabs/advanced-android-kotlin-training-canvas#5
 class CanvasFragment : Fragment() {
+
+    val bitmap = Bitmap.createBitmap(800, 600, Bitmap.Config.ARGB_8888);
+    val extrabitmap = Bitmap.createBitmap(800, 600, Bitmap.Config.ARGB_8888);
+    val canvas = Canvas(bitmap);
 
     companion object {
         fun newInstance() = CanvasFragment()
+        const val OFFSET = 0;
+        const val MULTIPLIER = 0;
     }
 
     private lateinit var viewModel: CanvasViewModel
--- mobile/app/src/main/java/com/example/prototype_mobile/view/game/GameActivity.kt
+++ mobile/app/src/main/java/com/example/prototype_mobile/view/game/GameActivity.kt
@@ -2,17 +2,27 @@ package com.example.prototype_mobile.view.game
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 import com.example.prototype_mobile.R
 
 class GameActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, CanvasFragment.newInstance())
-                    .commitNow()
-        }
+
+
+        val myCanvasView =
+            MyCanvasView(this)
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
+
+//        setContentView(R.layout.activity_game)
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                    .replace(R.id.container, CanvasFragment.newInstance())
+//                    .commitNow()
+//        }
     }
 }
