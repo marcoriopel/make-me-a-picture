899027b39c4f3d2361590ed21d0b99325e49f1cb
guthi1 <g.thibault@polymtl.ca>
2021-03-05 12:28:17 -0500

    Fix of newInstance()
    Former-commit-id: bc2bed11773e160fda0ce6bc42b43340a2215c29

--- mobile/app/src/main/java/com/example/prototype_mobile/view/game/GameActivity.kt
+++ mobile/app/src/main/java/com/example/prototype_mobile/view/game/GameActivity.kt
@@ -11,17 +11,10 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
-//        val myCanvasView =
-//            MyCanvasView(this)
-//        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
-//        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
-//        setContentView(myCanvasView)
-
         setContentView(R.layout.activity_game)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, CanvasFragment.newInstance())
+                    .replace(R.id.container, CanvasFragment())
                     .commitNow()
         }
     }
