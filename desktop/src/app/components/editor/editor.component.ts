ccb78d817223d83bf1dfe8b6a11dacadb3eee602
tetreaultmarc <43557279+tetreaultmarc@users.noreply.github.com>
2021-03-14 18:54:01 -0400

    fixed image creation and mouse hovering over canvas during game
    Former-commit-id: 38eeea5dcdea07c2166d2a963a4f0c73ef5e1e7d

--- desktop/src/app/components/editor/editor.component.ts
+++ desktop/src/app/components/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
 import { Vec2 } from '@app/classes/vec2';
 import { DrawingComponent } from '@app/components/drawing/drawing.component';
 import {
@@ -36,9 +36,10 @@ import { UndoRedoService } from '@app/services/undo-redo/undo-redo.service';
 export class EditorComponent implements AfterViewInit {
     @ViewChild('drawingComponent', { static: false }) drawingComponent: DrawingComponent;
     @ViewChild('workSpace', { static: false }) workSpaceRef: ElementRef<HTMLDivElement>;
-    @ViewChild('previewDiv', { static: false }) previewDivRef: ElementRef<HTMLDivElement>;
     @ViewChild('pencil', { static: false }) pencilRef: ElementRef<HTMLButtonElement>;
     @ViewChild('eraser', { static: false }) eraserRef: ElementRef<HTMLButtonElement>;
+
+    @Input() isInGame: boolean;
 
     lineWidthMin: number = MIN_LINE_WIDTH;
     lineWidthMax: number = MAX_LINE_WIDTH;
@@ -48,7 +49,6 @@ export class EditorComponent implements AfterViewInit {
     workSpaceSize: Vec2 = { x: MINIMUM_WORKSPACE_WIDTH, y: MINIMUM_WORKSPACE_HEIGHT };
     previewSize: Vec2 = { x: MINIMUM_CANVAS_WIDTH, y: MINIMUM_CANVAS_HEIGHT };
     canvasSize: Vec2 = { x: MINIMUM_CANVAS_WIDTH, y: MINIMUM_CANVAS_HEIGHT };
-    previewDiv: HTMLDivElement;
 
     shortcutsArray: string[] = ['c', 'z', 'Z', 'g'];
 
@@ -80,35 +80,14 @@ export class EditorComponent implements AfterViewInit {
     ngAfterViewInit(): void {
         setTimeout(() => {
             const workspaceElement: HTMLElement = this.workSpaceRef.nativeElement;
-            this.drawingService.gridCanvas.style.cursor = 'crosshair';
             this.workSpaceSize.x = workspaceElement.offsetWidth;
             this.workSpaceSize.y = workspaceElement.offsetHeight;
-            this.previewDiv = this.previewDivRef.nativeElement;
-            this.previewDiv.style.display = 'none';
-            this.previewDiv.style.position = 'absolute';
         });
-    }
-
-    onMouseDown(event: MouseEvent): void {
-        this.previewDiv.style.display = 'block';
     }
 
     @HostListener('document:keydown', ['$event'])
     onKeyDown(event: KeyboardEvent): void {
         this.hotkeyService.onKeyDown(event);
-    }
-
-    @HostListener('mousemove', ['$event'])
-    onMouseMove(event: MouseEvent): void {
-    }
-
-    @HostListener('mouseup', ['$event'])
-    onMouseUp(event: MouseEvent): void {
-        try{
-            this.previewDiv.style.display = 'none';
-        } catch(e) {
-            return;
-        }
     }
 
     setColor(color: string){
