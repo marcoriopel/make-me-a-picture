f8f047d7da7ca977afdb502778a9bc855bca1e7a
guthi1 <g.thibault@polymtl.ca>
2021-03-15 23:52:52 -0400

    Send guess
    Former-commit-id: ef393758822e3346d5e12c7706e6dfdaf9952dc4

--- mobile/app/src/main/java/com/example/prototype_mobile/model/game/GameRepository.kt
+++ mobile/app/src/main/java/com/example/prototype_mobile/model/game/GameRepository.kt
@@ -2,6 +2,11 @@ package com.example.prototype_mobile.model.game
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.prototype_mobile.GuessEvent
+import com.example.prototype_mobile.model.SocketOwner
+import com.google.gson.Gson
+
+const val GUESS_DRAWING_EVENT = "guessDrawing"
 
 class GameRepository {
     companion object {
@@ -19,13 +24,15 @@ class GameRepository {
         }
     }
 
+    var socket: io.socket.client.Socket
+    val gson: Gson = Gson()
+    var gameId: String? = null
+
     private val _isPlayerDrawing = MutableLiveData<Boolean>()
     val isPlayerDrawing: LiveData<Boolean> = _isPlayerDrawing
 
     private val _isPlayerGuessing = MutableLiveData<Boolean>()
     val isPlayerGuessing: LiveData<Boolean> = _isPlayerGuessing
-
-    var gameId: String? = null
 
     fun setIsPlayerDrawing(isDrawing: Boolean) {
         _isPlayerDrawing.value = isDrawing
@@ -35,9 +42,14 @@ class GameRepository {
         _isPlayerGuessing.value = isGuessing
     }
 
+    fun guessDrawing(guess: String) {
+        val guessEvent = GuessEvent(this.gameId!!, guess)
+        socket.emit(GUESS_DRAWING_EVENT, gson.toJson(guessEvent))
+    }
+
     init {
         _isPlayerDrawing.value = false
         _isPlayerGuessing.value = true
-
+        socket = SocketOwner.getInstance()!!.socket
     }
 }
--- mobile/app/src/main/java/com/example/prototype_mobile/viewmodel/game/GuessViewModel.kt
+++ mobile/app/src/main/java/com/example/prototype_mobile/viewmodel/game/GuessViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.prototype_mobile.viewmodel.game
 
 import androidx.lifecycle.ViewModel
+import com.example.prototype_mobile.model.game.GameRepository
 
 class GuessViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val gameRepo = GameRepository.getInstance()
+    
+    fun guessDrawing(guess: String) {
+        this.gameRepo!!.guessDrawing(guess)
+    }
+
 }
