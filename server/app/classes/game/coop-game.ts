51d0214fbb363fb0b2179bbd05a4ba55b300b93c
Marco <marco.riopel@hotmail.com>
2021-03-16 18:55:12 -0400

    add timer in coop game
    Former-commit-id: a1bcaad001d915edd3cf2735af13f0a9f6890f77

--- server/app/classes/game/coop-game.ts
+++ server/app/classes/game/coop-game.ts
@@ -1,5 +1,6 @@
 import { DrawingEvent } from '@app/ressources/interfaces/game-events';
 import { BasicUser, Player } from '@app/ressources/interfaces/user.interface';
+import { Difficulty } from '@app/ressources/variables/game-variables';
 import { DrawingsService } from '@app/services/drawings.service';
 import { SocketService } from '@app/services/sockets/socket.service';
 import { injectable } from 'inversify';
@@ -14,7 +15,10 @@ export class CoopGame extends Game {
     private vPlayer: VirtualPlayer;
     private score: number = 0;
     private guessesLeft: number = 0;
-    private round: number = 0;
+    private gameTimerCount: number = 0;
+    private gameTimerInterval: NodeJS.Timeout;
+    private drawingTimerCount: number = 0;
+    private drawingTimerInterval: NodeJS.Timeout;
     private currentDrawingName: string;
 
     constructor(lobby: CoopLobby, socketService: SocketService, private drawingsService: DrawingsService) {
@@ -26,59 +30,52 @@ export class CoopGame extends Game {
 
     async startGame(): Promise<void> {
         this.setGuesses();
-        this.round = 1;
         if (this.vPlayer != undefined) {
             this.vPlayer.setServices(this.drawingsService, this.socketService)
         }
         this.socketService.getSocket().to(this.id).emit('score', { "score": this.score });
         this.socketService.getSocket().to(this.id).emit('guessesLeft', { "guessesLeft": this.guessesLeft })
         this.currentDrawingName = await this.vPlayer.getNewDrawing(this.difficulty);
+        this.startGameTimer();
+        this.startDrawingTimer();
         this.vPlayer.startDrawing();
     }
 
     guessDrawing(username: string, guess: string): void {
-        if (this.players.has(username)) {
-            this.drawingTeamGuess(username, guess);
+        if (!this.players.has(username)) {
+            throw Error("User is not part of the game")
         }
-        this.socketService.getSocket().to(this.id).emit('guessesLeft', { "guessesLeft": this.guessesLeft })
-    }
-
-    private drawingTeamGuess(username: string, guess: string): void {
         if (this.currentDrawingName == guess) {
+            this.addBonusGameTime();
             ++this.score;
             this.socketService.getSocket().to(this.id).emit('guessCallback', { "isCorrectGuess": true, "guessingPlayer": username });
             this.socketService.getSocket().to(this.id).emit('score', { "score": this.score })
             this.vPlayer.stopDrawing();
-            this.setupNextRound();
+            this.setupNextDrawing();
         }
         else {
             this.socketService.getSocket().to(this.id).emit('guessCallback', { "isCorrectGuess": false, "guessingPlayer": username })
             --this.guessesLeft;
             if (!this.guessesLeft) {
                 this.vPlayer.stopDrawing();
-                this.setupNextRound();
+                this.setupNextDrawing();
             }
         }
+        this.socketService.getSocket().to(this.id).emit('guessesLeft', { "guessesLeft": this.guessesLeft })
     }
 
-    private selectRandomBinary(): number {
-        return Math.floor(Math.random() + 0.5);
-    }
 
-    private async setupNextRound() {
-        if (this.round < 4) {
-            ++this.round;
-            this.setGuesses();
-            this.socketService.getSocket().to(this.id).emit('round', { "round": this.round });
-            this.currentDrawingName = await this.vPlayer.getNewDrawing(this.difficulty);
-            this.vPlayer.startDrawing();
-        }
-        else {
-            this.endGame();
-        }
+    private async setupNextDrawing() {
+        clearInterval(this.drawingTimerInterval);
+        this.setGuesses();
+        this.currentDrawingName = await this.vPlayer.getNewDrawing(this.difficulty);
+        this.vPlayer.startDrawing();
+        this.startDrawingTimer();
+
     }
 
     private endGame(): void {
+        clearInterval(this.gameTimerInterval);
         this.guessesLeft = 0;
         this.socketService.getSocket().to(this.id).emit('endGame', { "finalScore": this.score });
         this.socketService.getSocket().to(this.id).emit('message', { "user": { username: "System" }, "text": "La partie est maintenant terminée!", "timeStamp": "timestamp", "textColor": "#2065d4", chatId: this.id });
@@ -93,6 +90,56 @@ export class CoopGame extends Game {
     }
 
     setGuesses(): void {
-        this.guessesLeft = 3;
+        switch (this.difficulty) {
+            case Difficulty.EASY:
+                this.guessesLeft = 5;
+                break;
+            case Difficulty.MEDIUM:
+                this.guessesLeft = 3;
+                break;
+            case Difficulty.HARD:
+                this.guessesLeft = 2;
+                break;
+        }
+    }
+
+    addBonusGameTime(): void {
+        switch (this.difficulty) {
+            case Difficulty.EASY:
+                this.gameTimerCount += 10;
+                break;
+            case Difficulty.MEDIUM:
+                this.gameTimerCount += 5;
+                break;
+            case Difficulty.HARD:
+                this.gameTimerCount += 3;
+                break;
+        }
+    }
+
+    startGameTimer() {
+        this.gameTimerCount = 60;
+        this.gameTimerInterval = setInterval(() => {
+            this.socketService.getSocket().to(this.id).emit('gameTimer', { "gameTimer": this.gameTimerCount });
+            if (!this.gameTimerCount) {
+                this.endGame;
+            }
+            else {
+                --this.gameTimerCount;
+            }
+        }, 1000);
+    }
+
+    startDrawingTimer() {
+        this.drawingTimerCount = 30;
+        this.drawingTimerInterval = setInterval(() => {
+            this.socketService.getSocket().to(this.id).emit('drawingTimer', { "drawingTimer": this.drawingTimerCount });
+            if (!this.drawingTimerCount) {
+                this.setupNextDrawing();
+            }
+            else {
+                --this.drawingTimerCount;
+            }
+        }, 1000);
     }
 }
