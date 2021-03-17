7d8555c399504ede44797fb4a2c59b223d911068
Marie-Eve Patron <marie-eve.patron@polymtl.ca>
2021-03-17 14:20:24 -0400

    canvas auth
    Former-commit-id: 118708bdb7546588075b0ebfb89cb8112682f32c

--- mobile/app/src/main/java/com/example/prototype_mobile/model/SocketOwner.kt
+++ mobile/app/src/main/java/com/example/prototype_mobile/model/SocketOwner.kt
@@ -27,7 +27,6 @@ class SocketOwner() {
 
     init {
         try {
-            val options = IO.Options()
             //This address is the way you can connect to localhost with AVD(Android Virtual Device)
             socket = IO.socket("http://18.217.235.167:3000/", options)
 //            socket = IO.socket("http://10.0.2.2:3000/")
@@ -38,12 +37,4 @@ class SocketOwner() {
         socket.connect()
     }
 
-    private val onTransport = Emitter.Listener { args ->
-        val transport: Transport = args[0] as Transport
-        transport.on(Transport.EVENT_REQUEST_HEADERS, Emitter.Listener { args ->
-            val headers = args[0] as MutableMap<String, List<String>>
-            val bearer = "bearer " + LoginRepository.getInstance()!!.user!!.token
-            headers["Authorization"] = Arrays.asList(bearer)
-        }).on(Transport.EVENT_RESPONSE_HEADERS, Emitter.Listener { })
-    }
 }
