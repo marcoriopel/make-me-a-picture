8786930d57f4ad163a85798798e378d55dc6cda5
Marie-Eve Patron <marie-eve.patron@polymtl.ca>
2021-03-17 13:12:52 -0400

    socket auth header
    Former-commit-id: f3778cd5624d9a178f8f0461bb76d731a0689cf9

--- mobile/app/src/main/java/com/example/prototype_mobile/model/SocketOwner.kt
+++ mobile/app/src/main/java/com/example/prototype_mobile/model/SocketOwner.kt
@@ -1,9 +1,11 @@
 package com.example.prototype_mobile.model
 
-import android.util.Log
-import com.google.gson.Gson
+import com.example.prototype_mobile.model.connection.login.LoginRepository
 import io.socket.client.IO
 import io.socket.emitter.Emitter
+import io.socket.engineio.client.Transport
+import java.util.*
+
 
 class SocketOwner() {
     companion object {
@@ -25,13 +27,23 @@ class SocketOwner() {
 
     init {
         try {
+            val options = IO.Options()
             //This address is the way you can connect to localhost with AVD(Android Virtual Device)
-            socket = IO.socket("http://18.217.235.167:3000/")
+            socket = IO.socket("http://18.217.235.167:3000/", options)
 //            socket = IO.socket("http://10.0.2.2:3000/")
 
         } catch (e: Exception) {
             e.printStackTrace()
         }
         socket.connect()
+    }
+
+    private val onTransport = Emitter.Listener { args ->
+        val transport: Transport = args[0] as Transport
+        transport.on(Transport.EVENT_REQUEST_HEADERS, Emitter.Listener { args ->
+            val headers = args[0] as MutableMap<String, List<String>>
+            val bearer = "bearer " + LoginRepository.getInstance()!!.user!!.token
+            headers["Authorization"] = Arrays.asList(bearer)
+        }).on(Transport.EVENT_RESPONSE_HEADERS, Emitter.Listener { })
     }
 }
