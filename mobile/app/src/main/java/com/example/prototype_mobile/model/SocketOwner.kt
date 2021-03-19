c81a5da1484bff4f6210b4d5239be0e4a35607e9
guthi1 <g.thibault@polymtl.ca>
2021-03-19 10:12:26 -0400

    Refactor auth in querry instead of header for socket
    Former-commit-id: 72a4df9854a2ce8b96e0067fa6475824787a4c5a

--- mobile/app/src/main/java/com/example/prototype_mobile/model/SocketOwner.kt
+++ mobile/app/src/main/java/com/example/prototype_mobile/model/SocketOwner.kt
@@ -1,7 +1,10 @@
 package com.example.prototype_mobile.model
 
+import android.util.Log
 import com.example.prototype_mobile.model.connection.login.LoginRepository
 import io.socket.client.IO
+import io.socket.emitter.Emitter
+import org.json.JSONObject
 import java.util.*
 
 class SocketOwner() {
@@ -22,18 +25,24 @@ class SocketOwner() {
 
     lateinit var socket: io.socket.client.Socket
 
+    private  var onErrorEvent = Emitter.Listener {
+        val msg = JSONObject(it[0].toString()).getString("error")
+        Log.e("Socket Error", msg.toString() )
+
+    }
+
     init {
         try {
             val opts = IO.Options()
-            opts.extraHeaders.set("authorization",  Arrays.asList(LoginRepository.getInstance()!!.user!!.token))
-            //This address is the way you can connect to localhost with AVD(Android Virtual Device)
-//            socket = IO.socket("http://18.217.235.167:3000/")
-            socket = IO.socket("http://10.0.2.2:3000/", opts)
+            opts.query = "authorization=" + LoginRepository.getInstance()!!.user!!.token
+            socket = IO.socket("http://18.217.235.167:3000/", opts)
+//            socket = IO.socket("http://10.0.2.2:3000/", opts)
 
         } catch (e: Exception) {
             e.printStackTrace()
         }
         socket.connect()
+        socket.on("error", onErrorEvent)
     }
 
 
