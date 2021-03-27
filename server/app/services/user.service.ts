ecfe8a8d302a04587b8aff7f1705fdd4364bee15
Samuel OUvrard <samuelroyouvrard@gmail.com>
2021-03-27 13:10:26 -0400

    get private stats now also returns a list of games played by the user
    Former-commit-id: 97933e1529d96f1f3dbc47d0d823704511637f91

--- server/app/services/user.service.ts
+++ server/app/services/user.service.ts
@@ -5,14 +5,16 @@ import { inject, injectable } from 'inversify';
 import { BasicUser } from '@app/ressources/interfaces/user.interface';
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from 'http-status-codes';
+import { GamesModel } from '@app/models/games.model';
 
 @injectable()
 export class UserService {
 
     constructor(
         @inject(TYPES.UsersModel) private usersModel: UsersModel,
-        @inject(TYPES.UserLogsModel) private userLogsModel: UserLogsModel) {
-    }
+        @inject(TYPES.UserLogsModel) private userLogsModel: UserLogsModel,
+        @inject(TYPES.GamesModel) private gamesModel: GamesModel ){
+        }
 
     async addUserToChat(username: string, chatId: string) {
         await this.usersModel.addUserToChat(username, chatId);
@@ -29,8 +31,9 @@ export class UserService {
         try{
             let userInfo = await this.usersModel.getUserInfo(username);
             let userLogs = await this.userLogsModel.getLogs(username);
+            let userGames = await this.gamesModel.getGames(username);
 
-            let privateInfo = {"name" : userInfo.name, "surname" : userInfo.surname, "logs": userLogs}
+            let privateInfo = {"name" : userInfo.name, "surname" : userInfo.surname, "logs": userLogs, "games" : userGames}
             next(privateInfo);
         }
         catch(e){
