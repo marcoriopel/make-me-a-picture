59f16e8167c86ada4b967b4bfe142c9184b4ead5
tetreaultmarc <43557279+tetreaultmarc@users.noreply.github.com>
2021-03-28 22:42:12 -0400

    added leaderboard logic with no ui
    Former-commit-id: 0ceb2307858282d4f670ad2f16bfe1239b71f96d

--- desktop/src/app/components/leaderboard/leaderboard.component.ts
+++ desktop/src/app/components/leaderboard/leaderboard.component.ts
@@ -1,15 +1,61 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
+import { LeaderboardCategory } from '@app/ressources/global-variables/global-variables';
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
   styleUrls: ['./leaderboard.component.scss']
 })
 export class LeaderboardComponent implements OnInit {
+  private getLeadboardStatsUrl = environment.api_url + '/api/stats/leaderboard';
+  category = new FormControl();
+  leaderboard: any = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'authorization': localStorage.getItem('token')!});
+    let params = new HttpParams();
+    params = params.append('category', LeaderboardCategory.BEST_SOLO_SCORE.toString());    
+    const options = { params: params, headers: headers};
+    this.http.get<any>(this.getLeadboardStatsUrl, options).subscribe((data: any) => {
+    })
+  }
+
+  updateLeaderboard(): void {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'authorization': localStorage.getItem('token')!});
+    let params = new HttpParams();
+    switch(this.category.value){
+      case LeaderboardCategory.MOST_GAMES_PLAYED.toString():
+        params = params.append('category', LeaderboardCategory.MOST_GAMES_PLAYED.toString());
+        break;
+
+      case LeaderboardCategory.MOST_TIME_PLAYED.toString():
+        params = params.append('category', LeaderboardCategory.MOST_TIME_PLAYED.toString());
+        break;
+
+      case LeaderboardCategory.BEST_SOLO_SCORE.toString():
+        params = params.append('category', LeaderboardCategory.BEST_SOLO_SCORE.toString());
+        break;  
+
+      case LeaderboardCategory.BEST_COOP_SCORE.toString():
+        params = params.append('category', LeaderboardCategory.BEST_COOP_SCORE.toString());
+        break;
+
+      case LeaderboardCategory.BEST_CLASSIC_WIN_RATIO.toString():
+        params = params.append('category', LeaderboardCategory.BEST_CLASSIC_WIN_RATIO.toString());
+        break;
+    }
+    const options = { params: params, headers: headers};
+    this.http.get<any>(this.getLeadboardStatsUrl, options).subscribe((data: any) => {
+      this.leaderboard = data.top10;
+    })
   }
 
 }
--- desktop/src/app/components/profile/profile.component.ts
+++ desktop/src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  private getUserInfoUrl = environment.api_url + '/api/user/private/info';
+  userInfo: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'authorization': localStorage.getItem('token')!});
+    const options = { headers: headers};
+    this.http.get<any>(this.getUserInfoUrl, options).subscribe((data: any) => {
+      console.log(data);
+    })
   }
 
 }
