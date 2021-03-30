cabfcb5d3c19b103b43a3c233a0cd65eea872c84
tetreaultmarc <43557279+tetreaultmarc@users.noreply.github.com>
2021-03-30 13:49:07 -0400

    basic profile
    Former-commit-id: 1309c250c28407ed9487a0fa408d7d3897a22dcd

--- desktop/src/app/components/profile/profile.component.ts
+++ desktop/src/app/components/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -8,19 +9,31 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  private getUserInfoUrl = environment.api_url + '/api/user/private/info';
+  private getUserInfoUrl = environment.api_url + '/api/stats/private';
+  category = new FormControl();
   userInfo: any;
+  avatar: number | null;
+  username: string | null;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+  }
 
   ngOnInit(): void {
+    let av = localStorage.getItem('avatar');
+    if(av) this.avatar = parseInt(av);
+    this.username = localStorage.getItem('username');
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'authorization': localStorage.getItem('token')!});
     const options = { headers: headers};
     this.http.get<any>(this.getUserInfoUrl, options).subscribe((data: any) => {
-      console.log(data);
+      this.userInfo = data.privateInfo;
+      console.log(this.userInfo);
     })
+  }
+
+  updateData(): void {
+    console.log(this.category.value)
   }
 
 }
