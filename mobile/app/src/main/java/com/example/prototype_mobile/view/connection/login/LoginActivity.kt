77b4643ed88dd20ec5982c30c866bf5318b6ebcb
Julien <julien.witty@polymtl.ca>
2021-04-03 00:27:26 -0400

    UI
    Former-commit-id: 34e7769ae6bcd8328c07e5a44f8d30a50a42b9e4

--- mobile/app/src/main/java/com/example/prototype_mobile/view/connection/login/LoginActivity.kt
+++ mobile/app/src/main/java/com/example/prototype_mobile/view/connection/login/LoginActivity.kt
@@ -17,6 +17,7 @@ import com.example.prototype_mobile.util.StringUtil
 import com.example.prototype_mobile.view.connection.sign_up.SignUpActivity
 import com.example.prototype_mobile.view.game.GameActivity
 import com.example.prototype_mobile.view.mainmenu.MainMenuActivity
+import com.example.prototype_mobile.view.tutorial.StaticTutorial
 import com.example.prototype_mobile.viewmodel.connection.login.LoginViewModel
 import com.example.prototype_mobile.viewmodel.connection.login.LoginViewModelFactory
 
@@ -89,8 +90,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun updateUiWithUser(username: String) {
-        val intent = Intent(this, MainMenuActivity::class.java);
-      // val intent = Intent(this, GameActivity::class.java)
+        //val intent = Intent(this, MainMenuActivity::class.java);
+       val intent = Intent(this, StaticTutorial::class.java)
         startActivity(intent)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
         Toast.makeText(
---
+++ mobile/app/src/main/java/com/example/prototype_mobile/view/tutorial/StaticTutorial.kt
@@ -0,0 +1,16 @@
+package com.example.prototype_mobile.view.tutorial
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.prototype_mobile.R
+
+class StaticTutorial : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_static_tutorial)
+
+
+        }
+}
