2405c6d27b49da17380732d79724339bc7c2ea7b
tetreaultmarc <43557279+tetreaultmarc@users.noreply.github.com>
2021-04-06 06:13:42 -0400

    fixed minor details
    Former-commit-id: 15ba943b688b074f3327c105e80bfaa3edc74497

--- desktop/src/app/components/join-private-game/join-private-game.component.ts
+++ desktop/src/app/components/join-private-game/join-private-game.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Game } from '@app/classes/game';
+import { GameService } from '@app/services/game/game.service';
 import { LobbyService } from '@app/services/lobby/lobby.service';
 
 @Component({
@@ -14,7 +16,7 @@ export class JoinPrivateGameComponent implements OnInit {
     id: '',
   })
 
-  constructor(private formBuilder: FormBuilder, private lobbyService: LobbyService, private snackBar: MatSnackBar) { }
+  constructor(private formBuilder: FormBuilder, private lobbyService: LobbyService, private snackBar: MatSnackBar, private gameService: GameService) { }
 
   ngOnInit(): void {
   }
@@ -23,7 +25,12 @@ export class JoinPrivateGameComponent implements OnInit {
     console.log(this.gameIdForm.value.id)
     this.lobbyService.joinPrivateGame(this.gameIdForm.value.id).subscribe(
       res => {
-        
+        // I need full game info here
+        this.lobbyService.oldLobbyId = "";
+        this.lobbyService.game.id = res.lobbyId;
+        this.gameService.gameId = res.lobbyId;
+        this.lobbyService.listen();
+        console.log(res)
       },
       err => {
         this.snackBar.open("Impossible de rejoindre la partie", "", {
