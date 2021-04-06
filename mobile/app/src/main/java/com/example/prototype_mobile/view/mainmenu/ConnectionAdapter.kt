9d0aeddb4ed8656712c5774238bc9a3f0e9cb93a
Marie-Eve Patron <marie-eve.patron@polymtl.ca>
2021-04-06 14:53:07 -0400

    Created the layout for the historics
    Former-commit-id: 0a811dc512f8ad53eb17df2c7c9a66d39ecf030e

---
+++ mobile/app/src/main/java/com/example/prototype_mobile/view/mainmenu/ConnectionAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.prototype_mobile.view.mainmenu
+
+class ConnectionAdapter {
+}
--- mobile/app/src/main/java/com/example/prototype_mobile/view/mainmenu/ProfilFragment.kt
+++ mobile/app/src/main/java/com/example/prototype_mobile/view/mainmenu/ProfilFragment.kt
@@ -6,11 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.prototype_mobile.R
+import com.example.prototype_mobile.databinding.FragmentGameCreationBinding
+import com.example.prototype_mobile.databinding.FragmentProfilBinding
 
 
 class ProfilFragment : Fragment() {
 
-
+    private lateinit var binding: FragmentProfilBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -21,6 +23,32 @@ class ProfilFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profil, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding = FragmentProfilBinding.bind(view)
+        val sectionArray = arrayOf(binding.profil1, binding.profil2, binding.statistic1, binding.gameRecyclerView, binding.connectionRecyclerView)
+        binding.toggleButtonGroup.addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
+            when(checkedId) {
+                R.id.profil -> {
+                    sectionArray.forEach { it.visibility = View.GONE }
+                    binding.profil1.visibility = View.VISIBLE
+                    binding.profil2.visibility = View.VISIBLE
+                }
+                R.id.statistic-> {
+                    sectionArray.forEach { it.visibility = View.GONE }
+                    binding.statistic1.visibility = View.VISIBLE
+                }
+                R.id.game_historic-> {
+                    sectionArray.forEach { it.visibility = View.GONE }
+                    binding.gameRecyclerView.visibility = View.VISIBLE
+                }
+                R.id.connection_historic-> {
+                    sectionArray.forEach { it.visibility = View.GONE }
+                    binding.connectionRecyclerView.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 
     companion object {
