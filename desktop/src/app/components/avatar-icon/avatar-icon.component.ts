584a5bf6e9bf57d2df25f8d4200a9673d1fc5ee2
Samuel OUvrard <samuelroyouvrard@gmail.com>
2021-04-07 14:24:55 -0400

    added ref to vPlayer images in desktop
    Former-commit-id: 760b0afc6818ae4498f6973b9c82edae7894527d

--- desktop/src/app/components/avatar-icon/avatar-icon.component.ts
+++ desktop/src/app/components/avatar-icon/avatar-icon.component.ts
@@ -29,8 +29,17 @@ export class AvatarIconComponent implements OnInit {
       case 4 :
         this.avatarImgRef += "avatar4.png"
         break;
-      case 5 :
-        this.avatarImgRef += "avatar5.png"
+      case 6 :
+        this.avatarImgRef += "profileVPlayer1.png"
+        break;
+      case 7 :
+        this.avatarImgRef += "profileVPlayer2.png"
+        break;
+      case 8 :
+        this.avatarImgRef += "profileVPlayer3.png"
+        break;
+      case 9 :
+        this.avatarImgRef += "profileVPlayer4.png"
         break;
       default:
         this.avatarImgRef += "profileIcon.png"
