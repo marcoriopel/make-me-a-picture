3b5bdcf5b15b35c03fb409a39653e192be0b951e
tetreaultmarc <43557279+tetreaultmarc@users.noreply.github.com>
2021-04-09 10:41:44 -0400

    UI improvements
    Former-commit-id: f3cda102121c61a16d5f582f6f58b53368f44d81

--- desktop/src/app/components/end-game-drawing/end-game-drawing.component.ts
+++ desktop/src/app/components/end-game-drawing/end-game-drawing.component.ts
@@ -11,6 +11,12 @@ export class EndGameDrawingComponent implements OnInit {
   virtualPlayerDrawings: string[] = [];
   realPlayerDrawings: string[] = [];
 
+  result: string = "";
+  winningTeam: any;
+  loosingTeam: any;
+  drawTeam1: any;
+  drawTeam2: any;
+
   constructor(private gameService: GameService) {
     this.virtualPlayerDrawings = this.gameService.virtualPlayerDrawings;
     this.realPlayerDrawings = this.realPlayerDrawings;
@@ -18,6 +24,52 @@ export class EndGameDrawingComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    if(this.gameService.score[0] > this.gameService.score[1]){
+      // Team 1 win
+      if(this.gameService.teams.team1.includes(this.gameService.username as string)){
+        this.result = 'Victoire'
+      }
+
+      this.winningTeam = {
+        score: this.gameService.score[0],
+        players: this.gameService.teams.team1,
+      }
+
+      this.loosingTeam = {
+        score: this.gameService.score[1],
+        players: this.gameService.teams.team2,
+      }
+
+    } else if(this.gameService.score[1] > this.gameService.score[0]){
+      // Team 2 win
+      if(this.gameService.teams.team1.includes(this.gameService.username as string)){
+        this.result = 'Défaite'
+      }
+
+      this.winningTeam = {
+        score: this.gameService.score[1],
+        players: this.gameService.teams.team2,
+      }
+
+      this.loosingTeam = {
+        score: this.gameService.score[0],
+        players: this.gameService.teams.team1,
+      }
+
+    } else {
+      // Draw
+      this.result = "Partie nulle";
+
+      this.drawTeam1 = {
+        score: this.gameService.score[0],
+        players: this.gameService.teams.team1,
+      }
+
+      this.drawTeam2 = {
+        score: this.gameService.score[1],
+        players: this.gameService.teams.team2,
+      }
+    }
   }
 
   upVote(): void {
