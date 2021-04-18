34fa94d9ce00133203580d96c6dd6bafb92c8d8e
Samuel OUvrard <samuelroyouvrard@gmail.com>
2021-04-18 16:57:04 -0400

    added ngOndestroy quit lobby handling
    Former-commit-id: db08e5981a6c508a93daf6105de09030df017a71

--- desktop/src/app/components/lobby/lobby.component.ts
+++ desktop/src/app/components/lobby/lobby.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from '@app/services/chat/chat.service';
+import { GameService } from '@app/services/game/game.service';
 import { LobbyService } from '@app/services/lobby/lobby.service';
 import { SocketService } from '@app/services/socket/socket.service';
 @Component({
@@ -14,10 +15,27 @@ export class LobbyComponent implements OnInit {
   coopImgRef: string = "./assets/img/hands-helping-solid.svg";
 
 
-  constructor(public lobbyService: LobbyService, private chatService: ChatService, private socketService: SocketService) { }
+  constructor(public lobbyService: LobbyService, private gameService: GameService, private chatService: ChatService, private socketService: SocketService) { }
 
   ngOnInit(): void {
     
+  }
+
+  ngOnDestroy(): void {
+    this.socketService.emit('leaveLobby', {'lobbyId': this.gameService.gameId});
+    this.lobbyService.quit();
+    this.chatService.setCurrentChat("General");
+    this.socketService.unbind('drawingEvent');
+    this.socketService.unbind('eraserStrokes');
+    this.socketService.unbind('transitionTimer');
+    this.socketService.unbind('drawingName');
+    this.socketService.unbind('timer');
+    this.socketService.unbind('newRound');
+    this.socketService.unbind('guessCallBack');
+    this.socketService.unbind('guessesLeft');
+    this.socketService.unbind('score');
+    this.socketService.unbind('gameStart');
+    this.socketService.unbind('endGame');
   }
 
   leaveLobby(): void {
