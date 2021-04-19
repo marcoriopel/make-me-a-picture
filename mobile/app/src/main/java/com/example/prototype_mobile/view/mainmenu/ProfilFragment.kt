b19f46981563cb2e2cc304336b3d43131042b775
guthi1 <g.thibault@polymtl.ca>
2021-04-19 14:20:16 -0400

    Added virtual player icon in lobby
    Former-commit-id: 8214b3a817171f93b8ce521ff4d32f8b772cfdc6

--- mobile/app/src/main/java/com/example/prototype_mobile/view/mainmenu/ProfilFragment.kt
+++ mobile/app/src/main/java/com/example/prototype_mobile/view/mainmenu/ProfilFragment.kt
@@ -35,10 +35,7 @@ class ProfilFragment : Fragment() {
         profilViewModel = ViewModelProvider(this, ProfilViewModelFactory()).get(ProfilViewModel::class.java)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profil, container, false)
     }
